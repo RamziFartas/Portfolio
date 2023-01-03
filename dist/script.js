13849a0c7de380b45c20dd82c463f24bc1a297ff
Ramzi Fartas <rfartas83@gmail.com>
2023-01-03 16:09:18 +0100

    modified:   dist/index.html 	modified:   dist/script.js

--- dist/script.js
+++ dist/script.js
@@ -1,13 +1,2 @@
-import anime from '/node_modules/animejs/lib/anime.es.js';
-
-const Bay = document.getElementById('profile');
-const hey = document.getElementById('avatar');
-console.log(hey);
-
-anime({
-    targets: Bay,
-    translateX:270,
-    duration: 5000,
-    easing: 'easeInOutExpo',
-    autoplay: true
- });
+const Icon = document.getElementById('profile');
+console.log(Icon);
