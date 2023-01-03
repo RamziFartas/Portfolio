202902e898045fdeb85921f5f3b1c151037bbac3
Ramzi Fartas <rfartas83@gmail.com>
2023-01-03 17:26:58 +0100

    modified:   dist/index.html 	modified:   dist/script.js 	modified:   dist/style.css

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,13 @@
-const Icon = document.getElementById('profile');
+const Icon = document.querySelector('#profile');
+const svg = document.querySelector('#profile-svg');
+const text = document.querySelector('#about-me');
+
 console.log(Icon);
+var tl = gsap.timeline({repeat:2, repeatDelay:1});
+tl.to(svg,{opacity:0,duration:1});
+tl.to(text,{opacity:0,duration:1});
+tl.to(Icon, {x: -20, duration: 1, ease:"power1"});
+tl.to(svg,{opacity:1,duration:1});
+tl.to(text,{opacity:1,duration:1});
+tl.to(Icon, {x: 0, duration: 1});
+
